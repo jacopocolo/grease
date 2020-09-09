d156d41285830a7d0110801081c594dd8d28c5dc
Jacopo Colò <nomedainserire@gmail.com>
2020-09-09 19:12:57 +0200

    Revert "Merge pull request #8 from jacopocolo/remove-canvas-entirely"
    This reverts commit c46225f6bbbed2ab4d75fe74d9e0c137ffb6180f, reversing
    changes made to 2e513d171d881ece0dce3aabdfacbd24488e8e50.

--- src/script.js
+++ src/script.js
@@ -107,10 +107,10 @@ var app = new Vue({
             else if (this.selectedTool == "select") {
                 app.selection.start();
             }
-            renderer.domElement.addEventListener("touchmove", this.onTapMove, false);
-            renderer.domElement.addEventListener("mousemove", this.onTapMove, false);
-            renderer.domElement.addEventListener("touchend", this.onTapEnd, false);
-            renderer.domElement.addEventListener("mouseup", this.onTapEnd, false);
+            drawingCanvas.addEventListener("touchmove", this.onTapMove, false);
+            drawingCanvas.addEventListener("mousemove", this.onTapMove, false);
+            drawingCanvas.addEventListener("touchend", this.onTapEnd, false);
+            drawingCanvas.addEventListener("mouseup", this.onTapEnd, false);
         },
         onTapMove: function (event) {
 
@@ -145,8 +145,8 @@ var app = new Vue({
             else if (this.selectedTool == "select") {
                 app.selection.end()
             }
-            renderer.domElement.removeEventListener("touchmove", this.onTapMove, false);
-            renderer.domElement.removeEventListener("mousemove", this.onTapMove, false);
+            drawingCanvas.removeEventListener("touchmove", this.onTapMove, false);
+            drawingCanvas.removeEventListener("mousemove", this.onTapMove, false);
         }
     },
     mounted() {
@@ -244,6 +244,10 @@ const encodedImageWidth = 800;
 let encodedImageHeigth = 300; //for safety
 const encodedImageDataStartingAt = 500;
 
+var drawingCanvas = document.getElementById("drawingCanvas");
+var context = drawingCanvas.getContext("2d");
+drawingCanvas.width = window.innerWidth;
+drawingCanvas.height = window.innerHeight;
 var main = document.getElementById("main");
 var miniAxis = document.getElementById("miniAxis");
 
@@ -431,55 +435,56 @@ let line = {
 };
 
 let eraser = {
-    sprites: [],
-    spriteMaterial: new THREE.SpriteMaterial({
-        map: new THREE.TextureLoader().load("../img/selector.png"),
-        opacity: 0.1,
-        color: new THREE.Color(0xFFFFFF),
-        //blending: THREE.MultiplyBlending,
-    }),
-    color: getComputedStyle(document.documentElement).getPropertyValue('--accent-color'),
     start: function () {
         raycaster = new THREE.Raycaster();
         raycaster.params.Line.threshold = threshold;
         raycaster.layers.set(1);
-        var sprite = new THREE.Sprite(this.spriteMaterial);
-        sprite.scale.set(0.05, 0.05, 0.05);
-        var vNow = new THREE.Vector3(mouse.tx, mouse.ty, 0);
-        vNow.unproject(camera);
-        sprite.position.set(vNow.x, vNow.y, vNow.z)
-        scene.add(sprite);
-        this.sprites.push(sprite);
+        paths.push([mouse.cx, mouse.cy]);
     },
     move: function () {
-        //Technically possible to load images from base 64 so
-        var sprite = new THREE.Sprite(this.spriteMaterial);
-        sprite.scale.set(0.05, 0.05, 0.05);
-        var vNow = new THREE.Vector3(mouse.tx, mouse.ty, 0);
-        vNow.unproject(camera);
-        sprite.position.set(vNow.x, vNow.y, vNow.z)
-        scene.add(sprite);
+        paths[paths.length - 1].push([mouse.cx, mouse.cy]);
+        //This is to render line transparency,
+        //we are redrawing the line every frame
+        this.redrawLine('rgba(255,255,255)');
 
-        this.sprites.push(sprite);
-        if (this.sprites.length > 10) {
-            scene.remove(this.sprites[0]);
-            this.sprites.shift()
-        }
         try {
             raycaster.setFromCamera(new THREE.Vector2(mouse.tx, mouse.ty), camera);
             var object = raycaster.intersectObjects(scene.children)[0].object;
-            scene.remove(object);
-            mirror.eraseMirrorOf(object);
-            object.dispose();
+            if (checkIfHelperObject(object)) {
+            } else {
+                //blueprint.removeFromBlueprint(object);
+                scene.remove(object);
+                mirror.eraseMirrorOf(object);
+                object.dispose();
+
+            }
         } catch (err) {
             //if there's an error here, it just means that the raycaster found nothing
         }
     },
     end: function () {
-        this.sprites.forEach(sprite => {
-            scene.remove(sprite);
-        })
-        this.sprites = [];
+        context.closePath();
+        context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+        paths = []
+    },
+    redrawLine: function (color) {
+        // clear canvas
+        context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+        context.strokeStyle = color;
+        context.globalAlpha = 0.25;
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+        context.lineWidth = 30;
+        context.beginPath();
+        for (var i = 0; i < paths.length; ++i) {
+            var path = paths[i];
+            if (path.length < 1)
+                continue;
+            context.moveTo(path[0][0], path[0][1]);
+            for (var j = 1; j < path.length; ++j)
+                context.lineTo(path[j][0], path[j][1]);
+        }
+        context.stroke();
     },
 };
 
@@ -491,51 +496,21 @@ app.selection = {
     group: undefined,
     transforming: false,
     raycaster: new THREE.Raycaster(),
-    sprites: [],
-    spriteMaterial: new THREE.SpriteMaterial({
-        map: new THREE.TextureLoader().load("../img/selector.png"),
-        opacity: 0.1,
-        color: new THREE.Color(getComputedStyle(document.documentElement).getPropertyValue('--accent-color-selected')),
-        //blending: THREE.MultiplyBlending,
-    }),
     color: getComputedStyle(document.documentElement).getPropertyValue('--accent-color'),
     start: function () {
         if (!this.transforming) {
-            //paths.push([mouse.cx, mouse.cy]);
+            paths.push([mouse.cx, mouse.cy]);
             this.raycaster = new THREE.Raycaster();
             this.raycaster.params.Line.threshold = threshold;
             this.raycaster.layers.set(1);
-
-            var sprite = new THREE.Sprite(this.spriteMaterial);
-            sprite.scale.set(0.05, 0.05, 0.05);
-            var vNow = new THREE.Vector3(mouse.tx, mouse.ty, 0);
-            vNow.unproject(camera);
-            sprite.position.set(vNow.x, vNow.y, vNow.z)
-            scene.add(sprite);
-            this.sprites.push(sprite);
-
         }
     },
     move: function () {
         if (!this.transforming && this.selected.length == 0) {
-
-            var sprite = new THREE.Sprite(this.spriteMaterial);
-            sprite.scale.set(0.05, 0.05, 0.05);
-            var vNow = new THREE.Vector3(mouse.tx, mouse.ty, 0);
-            vNow.unproject(camera);
-            sprite.position.set(vNow.x, vNow.y, vNow.z)
-            scene.add(sprite);
-
-            this.sprites.push(sprite);
-            if (this.sprites.length > 40) {
-                scene.remove(this.sprites[0]);
-                this.sprites.shift()
-            }
-            for (var i = this.sprites.length; i > 1; i = i - 1) {
-                var s = 0.05;
-                this.sprites[this.sprites.length - i].scale.set(s, s, s);
-            }
-
+            paths[paths.length - 1].push([mouse.cx, mouse.cy]);
+            //This is to render line transparency,
+            //we are redrawing the line every frame
+            this.redrawLine(this.color);
             try {
                 this.raycaster.setFromCamera(new THREE.Vector2(mouse.tx, mouse.ty), camera);
                 //scene.add(new THREE.ArrowHelper( this.raycaster.ray.direction, this.raycaster.ray.origin, 100, Math.random() * 0xffffff ));
@@ -551,11 +526,9 @@ app.selection = {
     },
     end: function () {
         if (!this.transforming) {
-            this.sprites.forEach(sprite => {
-                scene.remove(sprite);
-            })
-            this.sprites = [];
-
+            context.closePath();
+            context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+            paths = [];
             if (this.selection.length == 0 || this.selected.length > 0) {
                 this.deselect()
             } else {
@@ -623,7 +596,7 @@ app.selection = {
         if (selection.length == 1) {
             this.helper = new THREE.BoxHelper(selection[0], new THREE.Color(this.color));
             scene.add(this.helper);
-            transformControls = new TransformControls(camera, renderer.domElement);
+            transformControls = new TransformControls(camera, drawingCanvas);
             transformControls.attach(selection[0]);
             transformControls.addEventListener("mouseDown", function () {
                 app.selection.transforming = true;
@@ -666,7 +639,7 @@ app.selection = {
                 this.selected.push(element);
             })
             this.helper.update();
-            transformControls = new TransformControls(camera, renderer.domElement);
+            transformControls = new TransformControls(camera, drawingCanvas);
             transformControls.attach(this.group);
         }
         transformControls.addEventListener("mouseDown", function () {
@@ -747,6 +720,25 @@ app.selection = {
         center.divideScalar(count);
         return center;
     },
+    redrawLine: function (color) {
+        // clear canvas
+        context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+        context.strokeStyle = color;
+        context.globalAlpha = 0.25;
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+        context.lineWidth = 30;
+        context.beginPath();
+        for (var i = 0; i < paths.length; ++i) {
+            var path = paths[i];
+            if (path.length < 1)
+                continue;
+            context.moveTo(path[0][0], path[0][1]);
+            for (var j = 1; j < path.length; ++j)
+                context.lineTo(path[j][0], path[j][1]);
+        }
+        context.stroke();
+    }
 };
 
 let mirror = {
@@ -831,6 +823,7 @@ let importFrom = {
         document.body.appendChild(input);
 
         input.onchange = event => {
+
             app.modal.show = true;
             app.modal.mode = 'loader';
             app.modal.title = "Loading your drawing from image";
@@ -1375,7 +1368,7 @@ function init() {
     camera.zoom = 900;
     camera.position.set(0, 0, 2);
 
-    directControls = new OrbitControls(camera, renderer.domElement);
+    directControls = new OrbitControls(camera, drawingCanvas);
     directControls.enableRotate = false;
     directControls.enableZoom = true;
     directControls.enablePan = true;
@@ -1392,7 +1385,7 @@ function init() {
     controls.enableZoom = false;
     controls.enablePan = false;
     controls.saveState();
-    transformControls = new TransformControls(camera, renderer.domElement);
+    transformControls = new TransformControls(camera, drawingCanvas);
 
     miniAxisCamera = new THREE.OrthographicCamera();
     miniAxisCamera.position.copy(camera.position);
@@ -1407,8 +1400,8 @@ function init() {
     window.addEventListener("resize", onWindowResize);
     window.addEventListener("orientationchange", onWindowResize);
     onWindowResize();
-    renderer.domElement.addEventListener("touchstart", app.onTapStart, false);
-    renderer.domElement.addEventListener("mousedown", app.onTapStart, false);
+    drawingCanvas.addEventListener("touchstart", app.onTapStart, false);
+    drawingCanvas.addEventListener("mousedown", app.onTapStart, false);
 
     //EXTRA RENDERER ONLY FOR MAKING GIFS
     bufferRenderer = new THREE.WebGLRenderer({
@@ -1429,6 +1422,8 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+    drawingCanvas.width = window.innerWidth;
+    drawingCanvas.height = window.innerHeight;
 
     insetWidth = window.innerHeight / 4;
     insetHeight = window.innerHeight / 4;
